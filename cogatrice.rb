591056b66a1ae191022adaa6f27d375c63282ea5
muflax <mail@muflax.com>
2013-05-02 22:21:46 +0200

    use full 2min length

--- cogatrice.rb
+++ cogatrice.rb
@@ -121,8 +121,7 @@ tests = [
          StroopTest.new(:red, :green, :yellow, :blue, :magenta),
         ]
          
-# duration = 2*60 # per test
-duration = 10
+duration = 2*60 # per test
 
 tests.each do |test|
   test.run duration
